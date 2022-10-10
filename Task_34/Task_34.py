bdbebf90d4f3ea9287b1ff7e73c051d9f750887f
KoT <Kotto@student.su>
2022-10-10 19:55:47 +0400

    added task 34

---
+++ Task_34/Task_34.py
@@ -0,0 +1,21 @@
+# Задача 34. Даны два файла, в каждом из которых находится запись многочлена, приравненного к нулю. 
+# Задача - сформировать файл, содержащий сумму многочленов (суммируем подобные слагаемые).
+# Пример:
+# 1 Файл : 2*x2 + 4*x + 5 = 0
+# 2 Файл : 4*x2 + 7*x + 9 = 0
+# 3 Файл: (содержит результат) 6*x2 + 11*x + 14 = 0
+
+with open('poly_1.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('poly_2.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
+with open('poly_1.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+with open('poly_2.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
